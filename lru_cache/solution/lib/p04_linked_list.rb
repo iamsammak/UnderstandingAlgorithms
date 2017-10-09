87be5c67abf8884d755003fc427d6ae107e1dfe4
Samuel Mak <samuelswmak@gmail.com>
2017-10-08 21:43:06 -0700

    add new solution file for linked list

--- lru_cache/solution/lib/p04_linked_list.rb
+++ lru_cache/solution/lib/p04_linked_list.rb
@@ -1,4 +1,4 @@
-class Link
+class Node
   attr_accessor :key, :val, :next, :prev
 
   def initialize(key = nil, val = nil)
@@ -11,20 +11,29 @@ class Link
   def to_s
     "#{@key}: #{@val}"
   end
+
+  def remove
+    self.prev.next = self.next if self.prev
+    self.next.prev = self.prev if self.next
+    self.next = nil
+    self.prev = nil
+    self
+  end
+
 end
 
 class LinkedList
   include Enumerable
 
   def initialize
-    @head = Link.new
-    @tail = Link.new
+    @head = Node.new
+    @tail = Node.new
     @head.next = @tail
     @tail.prev = @head
   end
 
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    each_with_index { |node, j| return node if i == j }
     nil
   end
 
@@ -41,34 +50,39 @@ class LinkedList
   end
 
   def get(key)
-    each { |link| return link.val if link.key == key }
+    each { |node| return node.val if node.key == key }
     nil
   end
 
   def include?(key)
-    any? { |link| link.key == key }
+    any? { |node| node.key == key }
   end
 
-  def insert(key, val)
-    each { |link| return link.val = val if link.key == key }
+  def append(key, val)
+    new_node = Node.new(key, val)
 
-    new_link = Link.new(key, val)
+    @tail.prev.next = new_node
+    new_node.prev = @tail.prev
+    new_node.next = @tail
+    @tail.prev = new_node
 
-    @tail.prev.next = new_link
-    new_link.prev = @tail.prev
-    new_link.next = @tail
-    @tail.prev = new_link
+    new_node
+  end
 
-    new_link
+  def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        return node;
+      end
+    end
   end
 
   def remove(key)
-    each do |link|
-      if link.key == key
-        link.prev.next = link.next
-        link.next.prev = link.prev
-        link.next, link.prev = nil, nil
-        return link.val
+    each do |node|
+      if node.key == key
+        node.remove
+        return node.val
       end
     end
 
@@ -76,14 +90,14 @@ class LinkedList
   end
 
   def each
-    current_link = @head.next
-    until current_link == @tail
-      yield current_link
-      current_link = current_link.next
+    current_node = @head.next
+    until current_node == @tail
+      yield current_node
+      current_node = current_node.next
     end
   end
 
   def to_s
-    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(', ')
   end
 end
